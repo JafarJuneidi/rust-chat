ef4e09f4e9cc83a4d455c13e30e2d6ada52a11fd
jafar <aljuneidi.jafar@gmail.com>
2022-12-04 17:29:20 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::{
+    form::Form,
+    fs::{relative, FileServer},
+    response::stream::{Event, EventStream},
+    serde::{Deserialize, Serialize},
+    tokio::select,
+    tokio::sync::broadcast::{channel, error::RecvError, Sender},
+    Shutdown, State,
+};
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
+}
+
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+    let _res = queue.send(form.into_inner());
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg)
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")))
+}
